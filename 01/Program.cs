b05deec6fa4369d8e2fb11683b6b94645425fb06
Radu Enache <radu.enache@nagarro.com>
2021-12-01 09:37:06 +0200

    01

--- 01/Program.cs
+++ 01/Program.cs
@@ -3,6 +3,30 @@
 var file = System.IO.File.OpenText("input.txt");
 var input = file.ReadToEnd()
     .Split("\r\n")
+    .Select(l => int.Parse(l))
     .ToList();
 
-Console.WriteLine($"Hello, World!");
+
+int inc = 0;
+int previousMeasurement = -1;
+foreach (var measurement in input)
+{
+    if (previousMeasurement > 0 && previousMeasurement < measurement)
+    {
+        inc++;
+    }
+    previousMeasurement = measurement;
+}
+
+int inc2 = 0;
+for (int i = 0; i < input.Count() - 3; i++)
+{
+    int slidingWindow = input[i] + input[i + 1] + input[i + 2];
+    int slidingWindow1 = input[i + 1] + input[i + 2] + input[i + 3];
+    if (slidingWindow < slidingWindow1)
+    {
+        inc2++;
+    }
+}
+
+Console.WriteLine($"Inc: {inc}; Window Inc {inc2}");
