35b0c2e9904950e6283452205623cdcb0ffa1bc7
Radu Enache <radu.enache@nagarro.com>
2021-12-02 16:09:01 +0200

    03

--- 01/Program.cs
+++ 01/Program.cs
@@ -13,7 +13,7 @@ var part1 = input.Take(input.Count() - 1).Zip(input.Skip(1));
 Console.WriteLine($"P1: {part1.Count(zip => zip.First < zip.Second)}");
 
 // create pairs ...
-// a+b+c < b + c + d === a < d
+// a+b+c < b+c+d === a < d
 // "skip" the intermediaries and no need for "sums"
 var part2 = input.Take(input.Count() - 3).Zip(input.Skip(3));
 Console.WriteLine($"P2: {part2.Count(zip => zip.First < zip.Second)}");
---
+++ 03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\r\n")
+    .ToList();
+
+Console.WriteLine($"");
