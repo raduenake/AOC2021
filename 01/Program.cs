8783e03c570a5a3e73c773ea683948bdba0775f6
Radu Enache <radu.enache@nagarro.com>
2021-12-02 15:52:32 +0200

    Update day 01

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,32 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
 
 var file = System.IO.File.OpenText("input.txt");
 var input = file.ReadToEnd()
     .Split("\r\n")
     .Select(l => int.Parse(l))
-    .ToList();
+    .ToImmutableList();
 
+// create pairs ...
+// with a "skew" [0,1]; [1,2]; etc...
+var part1 = input.Take(input.Count() - 1).Zip(input.Skip(1));
+Console.WriteLine($"P1: {part1.Count(zip => zip.First < zip.Second)}");
 
-int inc = 0;
-int previousMeasurement = -1;
-foreach (var measurement in input)
-{
-    if (previousMeasurement > 0 && previousMeasurement < measurement)
-    {
-        inc++;
-    }
-    previousMeasurement = measurement;
-}
-
-int inc2 = 0;
-for (int i = 0; i < input.Count() - 3; i++)
-{
-    int slidingWindow = input[i] + input[i + 1] + input[i + 2];
-    int slidingWindow1 = input[i + 1] + input[i + 2] + input[i + 3];
-    if (slidingWindow < slidingWindow1)
-    {
-        inc2++;
-    }
-}
-
-Console.WriteLine($"Inc: {inc}; Window Inc {inc2}");
+// create pairs ...
+// a+b+c < b + c + d === a < d
+// "skip" the intermediaries and no need for "sums"
+var part2 = input.Take(input.Count() - 3).Zip(input.Skip(3));
+Console.WriteLine($"P2: {part2.Count(zip => zip.First < zip.Second)}");
