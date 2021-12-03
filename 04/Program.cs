4da1a6d4064fe2faa501e63f56683a7343359954
Radu Enache <radu.enache@nagarro.com>
2021-12-03 13:31:38 +0200

    04

---
+++ 04/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\r\n")
+    .ToImmutableList();
+
+var sw = new Stopwatch();
+sw.Start();
+// P1
+sw.Stop();
+
+Console.WriteLine($"[{sw.Elapsed}] P1: {0}");
+
+sw.Reset();
+sw.Start();
+// P2
+sw.Stop();
+
+Console.WriteLine($"[{sw.Elapsed}] P1: {0}");
