65cd135218ac0361eafd65aa99c021dd0b9891ef
Radu Enache <radu.enache@nagarro.com>
2021-12-07 07:39:30 +0200

    07

---
+++ 07/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split(',')
+    .Select(v => double.Parse(v))
+    .ToImmutableList();
+
+var minFuel = double.MaxValue;
+for (var i = input.Min(); i <= input.Max(); i++)
+{
+    var fuel = input.Aggregate((double)0, (acc, crab) =>
+    {
+        var distance = Math.Abs(crab - i);
+        // Part 1
+        // acc += distance;
+        // Part 2
+        acc += Math.Floor(distance * (distance + 1) / 2);
+
+        return acc;
+    });
+    if (fuel < minFuel)
+    {
+        minFuel = fuel;
+    }
+}
+
+Console.WriteLine($"Answer: {minFuel}");
