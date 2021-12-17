83f164a257cc9c1df85ac3b6532393e833f9df49
Radu Enache <radu.enache@nagarro.com>
2021-12-17 09:18:56 +0200

    17 refactor, clean code

--- 17/Program.cs
+++ 17/Program.cs
@@ -1,8 +1,8 @@
 ﻿// too lazy to pattern match and read the input ...
-var target = (x: (xmin: 201, xmax: 230), y: (ymin: -99, ymax: -65));
+var targetAreaInput = (X: (xMin: 201, xMax: 230), Y: (yMin: -99, yMax: -65));
 
-var sim = new Simulation(target);
-var hits = sim.RunSim().ToList();
+var simulation = new Simulation(targetAreaInput);
+var hits = simulation.RunSim().ToList();
 
-Console.WriteLine($"P1: {hits.OrderBy(r => r.y_max).Last().y_max}");
+Console.WriteLine($"P1: {hits.OrderBy(result => result.yMaxHit).Last().yMaxHit}");
 Console.WriteLine($"P2: {hits.Count()}");
--- 17/Simulation.cs
+++ 17/Simulation.cs
@@ -1,17 +1,17 @@
 class Simulation
 {
-    private ((int xmin, int xmax) x, (int ymin, int ymax) y) target;
-    public Simulation(((int xmin, int xmax) x, (int ymin, int ymax) y) tg)
+    private ((int xMin, int xMax) X, (int yMin, int yMax) Y) targetArea;
+    public Simulation(((int xMin, int xMax) X, (int yMin, int yMax) Y) _target)
     {
-        target = tg;
+        targetArea = _target;
     }
 
-    private (bool hit, int x, int y, int y_max) Simulate(int dx, int dy)
+    private (bool hit, int x, int y, int yMaxHit) Simulate(int dx, int dy)
     {
         bool hit = false;
         var x = 0;
         var y = 0;
-        var y_max = int.MinValue;
+        var yMaxHit = int.MinValue;
 
         while (true)
         {
@@ -19,36 +19,36 @@ class Simulation
             y += dy;
             dx -= dx > 0 ? 1 : dx < 0 ? -1 : 0;
             dy -= 1;
-            y_max = (y > y_max) switch
+            yMaxHit = (y > yMaxHit) switch
             {
                 true => y,
-                false => y_max
+                false => yMaxHit
             };
-            if (x >= target.x.xmin &&
-                x <= target.x.xmax &&
-                y >= target.y.ymin &&
-                y <= target.y.ymax)
+            if (x >= targetArea.X.xMin &&
+                x <= targetArea.X.xMax &&
+                y >= targetArea.Y.yMin &&
+                y <= targetArea.Y.yMax)
             {
                 hit = true;
                 break;
             }
-            if (y < target.y.ymin)
+            if (y < targetArea.Y.yMin)
             {
                 break;
             }
         }
-        return (hit, x, y, y_max);
+        return (hit, x, y, yMaxHit);
     }
 
-    public IEnumerable<(bool hit, int x, int y, int y_max)> RunSim()
+    public IEnumerable<(bool hit, int x, int y, int yMaxHit)> RunSim()
     {
         foreach (var dx in Enumerable.Range(-1000, 2000))
             foreach (var dy in Enumerable.Range(-1000, 2000))
             {
-                var r = Simulate(dx, dy);
-                if (r.hit)
+                var result = Simulate(dx, dy);
+                if (result.hit)
                 {
-                    yield return r;
+                    yield return result;
                 }
             }
     }
